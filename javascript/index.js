b2b412a0b3dfcd0cce1ccf6f8f24521e35c042e8
tech-taku <taku.map25@gmail.com>
2024-04-08 03:03:51 +0200

    Add Etch-a-Sketch with grid size manipulation and randomly generated colors.

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,54 @@
-document.addEventListener("DOMContentLoaded", createContainer(size))
+document.addEventListener("DOMContentLoaded", function () {
+    let popUpButton = document.querySelector("#popup");
+    popUpButton.addEventListener("click", function () {
+        const size = getSize()
+        createContainer(size);
+    })
 
-const generateRandomColor  = () => {
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.florr(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
+})
 
-
-    const color = "rgb(red, green, blue)";
-}
-
-
-const createContainer = (size) => {
+function createContainer(size) {
 
     let container = document.querySelector(".container");
 
-    container.style.gridTemplateColumns = `${size}`;
-    container.style.gridTemplateRows = `${size}`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    let numDivs = size * size
 
-    for (let i = 0 ; i , size * size; i++){
+    for (let i = 0; i < numDivs; i++) {
         let divBlock = document.createElement("div")
-        container.style.backgroundColor = generateRandomColor();
+        divBlock.addEventListener("mouseover", () => {
+            divBlock.style.backgroundColor = generateRandomColor();
+        })
         container.insertAdjacentElement("beforeend", divBlock)
     }
 }
+
+function getSize() {
+    let input = prompt("What your desired grid size? (Valid Number)");
+    let message = document.querySelector("#message");
+
+    if (input === "") {
+        message.textContent = "Please provide a number"
+
+    } else if (input < 0 || input > 100) {
+        message.textContent = "Provide a number between 1 and 100";
+
+    } else {
+        message.textContent = "Go Ahead and Play.";
+    }
+    return input;
+}
+function generateRandomColor() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+
+    const color = `rgb(${red}, ${green}, ${blue})`;
+
+    return color
+}
+
+
